ad77de3999654c268dc9b826a3cd8a4bb39fee92
Yatin Kumar <yatin.2022ug1074@iiitranchi.ac.in>
2025-01-06 21:57:42 +0530

    Patients Page Complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Appointments from './pages/Appointments'
 import Doctors from './pages/Doctors.jsx'
+import Patients from './pages/Patients.jsx'
 
 function App() {
 
   return (
     <>
-       <Doctors/>
+    <Patients/>
     </>
   )
 }
