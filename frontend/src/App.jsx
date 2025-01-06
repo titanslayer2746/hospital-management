982c38772451bac4cbed48978e06a984c9fffbcf
Yatin Kumar <yatin.2022ug1074@iiitranchi.ac.in>
2025-01-06 22:43:06 +0530

    Project Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,64 @@
+//App.js
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes, Route,
+    Link, useNavigate
+} from 'react-router-dom';
+import Appointments from './pages/Appointments.jsx';
+import Doctors from './pages/Doctors.jsx';
+import Patients from './pages/Patients.jsx';
 import './App.css'
-import Appointments from './pages/Appointments'
-import Doctors from './pages/Doctors.jsx'
-import Patients from './pages/Patients.jsx'
 
-function App() {
+const App = () => {
+    const isLinkActive =
+        (path) =>
+            window.location.pathname === path;
+    return (
+        <Router>
+            <div className="container">
+                <h1 style={{ color: "green" }}>
+                    Hospital Managment App
+                </h1>
+                <nav>
+                    <ul>
+                        <li className={
+                            isLinkActive('/appointments')
+                                ? 'active' : ''}>
+                            <Link to="/appointments">
+                                Appointments
+                            </Link>
+                        </li>
+                        <li className={
+                            isLinkActive('/doctors') ?
+                                'active' : ''}>
+                            <Link to="/doctors">
+                                Doctors
+                            </Link>
+                        </li>
+                        <li className={
+                            isLinkActive('/patients') ?
+                                'active' : ''}>
+                            <Link to="/patients">
+                                Patients
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
 
-  return (
-    <>
-    <Patients/>
-    </>
-  )
+                <Routes>
+                    <Route path="/appointments"
+                        element={<Appointments />} />
+                    <Route path="/"
+                        element={<Appointments />} />
+                    <Route path="/doctors"
+                        element={<Doctors />} />
+                    <Route path="/patients"
+                        element={<Patients />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
