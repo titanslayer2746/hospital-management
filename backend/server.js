966ce4d50755dd3533b8af89cdea8dd56b8c399a
Yatin Kumar <yatin.2022ug1074@iiitranchi.ac.in>
2025-05-09 17:13:23 +0530

    deployment bug fix

--- backend/server.js
+++ backend/server.js
@@ -26,14 +26,6 @@ app.use(bodyParser.json()); // Parse JSON request bodies
 // Connect to the database
 connectDB()
 
-// MongoDB connection configuration
-mongoose.connect('mongodb://localhost:27017/hospital-management', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
-
 // API Routes
 app.use('/api/patients', patientsRouter); // Patient management routes
 app.use('/api/doctors', doctorsRouter); // Doctor management routes
